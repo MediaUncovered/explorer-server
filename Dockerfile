ef75f737cec0d37eb2652ac8b5733bd6f721f263
Theo van den Bogaart <todorus@todorus.com>
2018-06-09 20:10:52 +0200

    written tests for querying using a small seed model

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 FROM python:3.6.5
 
 COPY ./app /flask-explorer/app
-COPY ./requirements.txt /flask-explorer/requirements.txt
 COPY ./server.py /flask-explorer/server.py
 
 WORKDIR /flask-explorer
+
+COPY ./requirements.txt /flask-explorer/requirements.txt
 RUN pip install -r requirements.txt
+COPY ./lib/newsanalysis-0.1.0-py3-none-any.whl /flask-explorer
+RUN pip install newsanalysis-0.1.0-py3-none-any.whl
 
 EXPOSE 5000
 CMD FLASK_APP=server.py flask run --host 0.0.0.0
--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,25 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify, request
+from newsAnalysis.Model import Model
 
 
-def create_app():
+def create_app(model_path=None):
     app = Flask(__name__)
 
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    if model_path is None:
+        model_path = os.path.abspath("./models/model")
+
+    app.model = Model(name="model", modelType="fasttext", model_path=model_path)
+    app.model.load()
 
     @app.route('/query/<string:word>')
     def query(word):
-        pass
+        count = int(request.args.get('count', 10))
+
+        similarWords = app.model.word_embedding.wv.similar_by_word(word, topn=count)
+        body = [{"label": result[0], "value": result[1]} for result in similarWords]
+
+        return jsonify(body)
 
     return app
