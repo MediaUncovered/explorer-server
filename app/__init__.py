3b8ca19b8ba46e05a2880e7da65f8d8633917290
tilana <NatalieWidmann@gmx.de>
2018-09-22 10:38:38 +0200

    update model, functionality to get model information

--- app/__init__.py
+++ app/__init__.py
@@ -13,9 +13,10 @@ def create_app():
 
     app.model = Model().load(model_path=config.MODEL_PATH)
 
+
     @app.route('/query/<string:word>')
     def query(word):
-        count = int(request.args.get('count', 10))
+        count = int(request.args.get('count', 30))
 
         similarWords = app.model.word_embedding.wv.similar_by_word(word, topn=count)
         body = [{"label": result[0], "value": result[1]} for result in similarWords]
@@ -32,5 +33,14 @@ def create_app():
         mapping = app.model.keywordMapping(data['keywords'], data['left'], data['right'])
         body = {'mapping': list(mapping)}
         return jsonify(body)
+
+    @app.route('/modelInfo')
+    def modelInfo():
+        return app.model.modelInfo.toJson()
+
+    @app.route('/analogies', methods=['POST'])
+    def generateAnalogies():
+        data = [{'x':'x', 'y':'y', 'score':0.75}, {'x':'x2', 'y':'y2', 'score':0.33}]
+        return jsonify(data)
 
     return app
