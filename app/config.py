43a1ced785b58feb58cdb584eb3939734d71e9b5
Theo van den Bogaart <todorus@todorus.com>
2018-09-29 22:27:45 +0200

    model index

--- app/config.py
+++ app/config.py
@@ -11,11 +11,12 @@ class ModelConfig(object):
     _models = None
 
     def models(self):
-        models = [model["label"] for model in self._models]
-        return models
+        # models = [model for model in self._models]
+        return list(self._models.keys())
 
     def load_model(self, model_name):
-        model_path = os.path.join(self._models_path, model_name)
+        model_file_name = self._models[model_name]["file"]
+        model_path = os.path.join(self._models_path, model_file_name)
         model = Model().load(model_path=model_path)
         return model
 
@@ -26,10 +27,11 @@ MODELS_CONFIG_PATH = os.getenv('MODELS_CONFIG_PATH', os.path.join(MODELS_PATH, "
 
 def load_model_config():
     stream = open(MODELS_CONFIG_PATH, "r")
-    models = yaml.load_all(stream)
+    models_raw = yaml.load_all(stream)
+    models = [model for model in models_raw]
 
     model_config = ModelConfig()
-    model_config._models = models
+    model_config._models = models[0]
     model_config._models_path = MODELS_PATH
 
     return model_config
