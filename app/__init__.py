c851e0e6a76378e21e682d3e60d3d343d6900a46
tilana <NatalieWidmann@gmx.de>
2018-10-03 09:43:54 +0200

    load model with set function to speed up loading time

--- app/__init__.py
+++ app/__init__.py
@@ -12,45 +12,46 @@ def create_app(model_config):
 
     CORS(app)
 
-    def get_model(model_name):
-        return model_config.load_model(model_name)
+    @app.route('/models/<string:model_name>')
+    def set_model(model_name):
+        app.model = model_config.load_model(model_name)
+        return '{}'
 
     @app.route('/models')
     def index():
         body = model_config.models()
         return jsonify(body)
 
-    @app.route('/models/<string:model_name>/query/<string:word>')
-    def query(model_name, word):
+    @app.route('/models/query/<string:word>')
+    def query(word):
         count = int(request.args.get('count', 30))
-
-        similarWords = get_model(model_name).word_embedding.wv.similar_by_word(word, topn=count)
+        similarWords = app.model.word_embedding.wv.similar_by_word(word, topn=count)
         body = [{"label": result[0], "value": result[1]} for result in similarWords]
 
         return jsonify(body)
 
-    @app.route('/models/<string:model_name>/info')
-    def info(model_name):
-        return get_model(model_name).collectionInfo.toJson()
+    @app.route('/models/info')
+    def info():
+        return app.model.collectionInfo.toJson()
 
-    @app.route('/models/<string:model_name>/reliability')
-    def reliability(model_name):
-        body = [{"section": elem['section'], "nr_total": elem["nr_total"], "nr_correct": elem["nr_correct"]} for elem in get_model(model_name).accuracy]
+    @app.route('/models/reliability')
+    def reliability():
+        body = [{"section": elem['section'], "nr_total": elem["nr_total"], "nr_correct": elem["nr_correct"]} for elem in app.model.accuracy]
         return jsonify(body)
 
-    @app.route('/models/<string:model_name>/keywordMapping', methods=['POST'])
-    def keywordMapping(model_name):
+    @app.route('/models/keywordMapping', methods=['POST'])
+    def keywordMapping():
         data = request.get_json()
-        mapping = get_model(model_name).keywordMapping(data['keywords'], data['left'], data['right'])
+        mapping = app.model.keywordMapping(data['keywords'], data['left'], data['right'])
         body = {'mapping': list(mapping)}
         return jsonify(body)
 
-    @app.route('/models/<string:model_name>/modelInfo')
-    def modelInfo(model_name):
-        return get_model(model_name).modelInfo.toJson()
+    @app.route('/models/modelInfo')
+    def modelInfo():
+        return app.model.modelInfo.toJson()
 
     @app.route('/analogies', methods=['POST'])
-    def generateAnalogies(model_name):
+    def generateAnalogies():
         data = [{'x':'x', 'y':'y', 'score':0.75}, {'x':'x2', 'y':'y2', 'score':0.33}]
         return jsonify(data)
 
